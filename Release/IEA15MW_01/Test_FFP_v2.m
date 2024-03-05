f6a995b72af166ca097c0d9d98794d6c90608e31
weifus <wei.fu@sowento.com>
2024-03-05 16:54:57 +0100

    set up test case

---
+++ Release/IEA15MW_01/Test_FFP_v2.m
@@ -0,0 +1,83 @@
+% IEA15MW_01: IEA 15 MW monopile + perfect wind preview from a single point 
+% lidar system.
+% Origin and changes in files: see ChangeLog.txt.
+% Purpose:
+% Here, we use a perfect wind preview to demonstrate that the collective
+% pitch feedforward controller (designed with SLOW) is able to reduce
+% significantly the rotor speed variation when OpenFAST is disturbed by an
+% Extreme Operating Gust. Here, only the rotational GenDOF is enabled.  
+% Result:       
+% Change in rotor over speed:  -96.9 %
+% Authors: 		
+% David Schlipf, Feng Guo
+
+%% Setup
+clearvars;close all;clc;
+addpath(genpath('..\WetiMatlabFunctions'))
+
+% Copy the adequate OpenFAST version to the example folder
+FASTexeFile     = 'openfast_x64.exe';
+FASTmapFile     = 'MAP_x64.dll';
+SimulationName  = 'IEA-15-240-RWT-Monopile';
+copyfile(['..\OpenFAST\',FASTexeFile],FASTexeFile)
+
+%% Run FB
+% ManipulateTXTFile('ROSCO_v2d6.IN','1 ! FlagLAC','0 ! FlagLAC');     % disable LAC
+% dos([FASTexeFile,' ',SimulationName,'.fst']);                       % run OpenFAST
+% movefile([SimulationName,'.outb'],[SimulationName,'_FB.outb'])      % store results
+
+%% Run FBFF  
+% ManipulateTXTFile('ROSCO_v2d6.IN','0 ! FlagLAC','1 ! FlagLAC');     % enable LAC
+dos([FASTexeFile,' ',SimulationName,'.fst']);                       % run OpenFAST
+movefile([SimulationName,'.outb'],[SimulationName,'_FBFF.outb'])    % store results
+
+%% Clean up
+delete(FASTexeFile)
+
+%% Comparison
+% read in data
+% FB              = ReadFASTbinaryIntoStruct([SimulationName,'_FB.outb']);
+FBFF            = ReadFASTbinaryIntoStruct([SimulationName,'_FBFF.outb']);
+R_FBFF              = ReadROSCOtextIntoStruct([SimulationName,'.RO.dbg']);
+
+% Plot 
+figure('Name','Simulation results')
+
+subplot(4,1,1);
+hold on; grid on; box on
+plot(FBFF.Time,       FBFF.Wind1VelX);
+plot(FBFF.Time,     FBFF.VLOS01LI);
+plot(R_FBFF.Time,     R_FBFF.REWS_f);
+% legend('Hub height wind speed','Vlos')
+ylabel('[m/s]');
+legend('Wind1VelX','VLOS01LI','REWS_f')
+
+subplot(4,1,2);
+hold on; grid on; box on
+% plot(FB.Time,       FB.BldPitch1);
+plot(FBFF.Time,     FBFF.BldPitch1);
+ylabel({'BldPitch1'; '[deg]'});
+% legend('feedback only','feedback-feedforward')
+
+subplot(4,1,3);
+hold on; grid on; box on
+% plot(FB.Time,       FB.RotSpeed);
+plot(FBFF.Time,     FBFF.RotSpeed);
+ylabel({'RotSpeed';'[rpm]'});
+
+subplot(4,1,4);
+hold on; grid on; box on
+% plot(FB.Time,       FB.TwrBsMyt/1e3);
+plot(FBFF.Time,     FBFF.TwrBsMyt/1e3);
+ylabel({'TwrBsMyt';'[MNm]'});
+
+xlabel('time [s]')
+linkaxes(findobj(gcf, 'Type', 'Axes'),'x');
+xlim([0 30])
+
+% % display results
+% RatedRotorSpeed = 7.56; % [rpm]
+% t_Start         = 0;    % [s]
+% fprintf('Change in rotor over speed:  %4.1f %%\n',...
+%     (max(abs(FBFF.RotSpeed(FBFF.Time>=t_Start)-RatedRotorSpeed))/...
+%      max(abs(FB.RotSpeed  (FB.Time  >=t_Start)-RatedRotorSpeed))-1)*100)
